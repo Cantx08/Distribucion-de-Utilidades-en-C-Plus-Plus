785c5f66c50e44af32d362e7c84780b5da8c6603
Cantx08 <edwin.cantuna@epn.edu.ec>
2024-03-26 19:29:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CHAVES ANÍVAR - DISEÑO DE ALGORITMOS
+## 4.1 Estructuras Secuenciales - Ejercicios Propuestos
+### Ejercicio 15. Las utilidades de una empresa se distribuyen entre tres socios así: socio A = 40%, socio B = 25% y socio C = 35%. Dada una cantidad de dinero ¿cuánto corresponderá a cada uno?
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "15 Programa que indica el valor correspondiente a las utilidades de cada socio";
+    float socioA, socioB, socioC, valor;
+
+    // Los utilidades deben ser mayores a 0
+    cout << "\nDigite el valor total de las utilidades: USD ";
+    cin >> valor;
+    // Al socio A le corresponde el 40% de las utilidades, al socio B el 25% y al socio C el 35%
+    socioA = 0.4 * valor;
+    socioB = 0.25 * valor;
+    socioC = 0.35 * valor;
+
+    cout << "\n***** DISTRIBUCION DE UTILIDADES *****";
+    cout << "\nSocio A: USD " << socioA;
+    cout << "\nSocio B: USD " << socioB;
+    cout << "\nSocio C: USD " << socioC;
+    return 0;
+}
